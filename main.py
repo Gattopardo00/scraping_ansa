f215c72b09431ce8e420f144220c0ff11ddfbf3e
Gattopardo00 <mariano.muscatello@gmail.com>
2022-04-04 21:10:16 +0200

    Update main.py
    aggiunte alcune righe per rendere più generico possibile il programma

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import requests
 from bs4 import BeautifulSoup as bs
+
+def quantita():
+    n = int(input("quante notizie vuoi?"))
+    return n
+
 def scrivi():
+    n=quantita()
     file = open("prova.txt","w")
     rq = requests.get("https://www.ansa.it")
     test=bs(rq.text,features="html.parser")
     test2=test.findAll("h3",{"class":"news-title"})
-    for test in test2[:5]:
+    for test in test2[:n]:
         print(test.text)
         file.write(test.text)
         file.write("\n")
     file.close()
 def scelta():
-   print("ciao")
+    
 while True:
     print('''
         Inserire cosa fare:
-        1) scrivere le prime 5 notizie di ansa.it
+        1) scegli e scrivi quante notizie vuoi di ansa.it
         2) scegliere una parola e vedere se è presente nelle prime 5 notizie di ansa.it
         3) uscire dal programma
     ''')
@@ -26,4 +32,5 @@ while True:
         scelta()
     elif a == "3":
         break
+
 
