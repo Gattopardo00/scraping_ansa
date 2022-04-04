09446b770448d671c90c284d0b3e3da4290d802f
Gattopardo00 <mariano.muscatello@gmail.com>
2022-04-04 21:45:53 +0200

    Update main.py
    aggiunta la funzione che ti aiuta a cercare in base alla parola il titolo e scrivere solo i titoli inerenti

--- main.py
+++ main.py
@@ -4,20 +4,32 @@ from bs4 import BeautifulSoup as bs
 def quantita():
     n = int(input("quante notizie vuoi?"))
     return n
-
 def scrivi():
     n=quantita()
-    file = open("prova.txt","w")
     rq = requests.get("https://www.ansa.it")
     test=bs(rq.text,features="html.parser")
-    test2=test.findAll("h3",{"class":"news-title"})
+    test2 = test.findAll("h3", {"class": "news-title"})
+    file = open("prova.txt", "w")
     for test in test2[:n]:
-        print(test.text)
         file.write(test.text)
         file.write("\n")
     file.close()
+    return test.text
 def scelta():
-    
+    file = open("prova.txt", "w")
+    a = input("inserisci la parola chiave")
+    n = quantita()
+    rq = requests.get("https://www.ansa.it")
+    test = bs(rq.text, features="html.parser")
+    test2 = test.findAll("h3", {"class": "news-title"})
+    file = open("prova.txt", "w")
+    for test in test2[:n]:
+        i = test.text.find(a)
+        if i != -1:
+            print ("prova33")
+            file.write(test.text)
+            file.write("\n")
+    file.close()
 while True:
     print('''
         Inserire cosa fare:
@@ -25,12 +37,11 @@ while True:
         2) scegliere una parola e vedere se è presente nelle prime 5 notizie di ansa.it
         3) uscire dal programma
     ''')
-    a=input()
+    a = input()
     if a == "1":
         scrivi()
     elif a == "2":
         scelta()
     elif a == "3":
         break
-
 
